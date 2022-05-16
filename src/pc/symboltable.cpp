7c51a5412ebc965683a4b837adbfcd2c72db3616
7rillium <3190105730@zju.edu.cn>
2022-05-15 21:20:25 -0700

    modify enterScope and leaveScope of symbol table

--- src/pc/symboltable.cpp
+++ src/pc/symboltable.cpp
@@ -1,79 +1,159 @@
 #include "symboltable.hpp"
 using namespace std;
 
-void SymbolTable::addSymbol(treeNode* t, int kind) {
+void SymbolTable::addSymbol(treeNode* t, int kind){
     unordered_map<string, list<TableItem>>* map;
-    switch (kind) {
-        case DK_Proc: map = &ProcDeclMap; break;
-        case DK_Func: map = &FuncDeclMap; break;
-        case DK_Var: map = &VarDeclMap; break;
-        case DK_Type: map = &TypeDeclMap; break;
-        default: return;
+    switch (kind)
+    {
+    case DK_Proc:
+        map=&ProcDeclMap;
+        break;
+    case DK_Func:
+        map=&FuncDeclMap;
+        break;
+    case DK_Var:
+        map=&VarDeclMap;
+        break;
+    case DK_Type:
+        map=&TypeDeclMap;
+        break;
+    default:
+        return;
     }
-    string name = t->getChild().at(0)->getIdName();
-    int    kind = t->getChild().at(1)->getNodeKind();
-    unordered_map<string, list<TableItem>>::iterator iter = map->find(name);
-    if (iter == map->end()) {
+    string name=t->getChild().at(0)->getIdName();
+    int kind=t->getChild().at(1)->getNodeKind();
+    unordered_map<string, list<TableItem> >::iterator iter=map->find(name);
+    if(iter==map->end()){
         map->insert(make_pair(name, list<TableItem>()));
-        iter = map->find(name);
+        iter=map->find(name);
+    }
+    if(iter->second.size()>0 && iter->second.front().level==currLevel){
+        err.add(REDEFINE, name, t->getLine());
+        return;
     }
     iter->second.push_front(TableItem(currLevel, name, kind, t));
 }
 
-TableItem* SymbolTable::findSymbol(string name, int kind) {
+TableItem* SymbolTable::findSymbol(string name, int kind){
     unordered_map<string, list<TableItem>>* map;
-    switch (kind) {
-        case DK_Proc: map = &ProcDeclMap; break;
-        case DK_Func: map = &FuncDeclMap; break;
-        case DK_Var: map = &VarDeclMap; break;
-        case DK_Type: map = &TypeDeclMap; break;
-        default: return nullptr;
-    }
-    unordered_map<string, list<TableItem>>::iterator iter = map->find(name);
-    if (iter == map->end()) {
+    switch (kind)
+    {
+    case DK_Proc:
+        map=&ProcDeclMap;
+        break;
+    case DK_Func:
+        map=&FuncDeclMap;
+        break;
+    case DK_Var:
+        map=&VarDeclMap;
+        break;
+    case DK_Type:
+        map=&TypeDeclMap;
+        break;
+    default:
         return nullptr;
-    } else {
+    }
+    unordered_map<string, list<TableItem> >::iterator iter=map->find(name);
+    if(iter==map->end()){
+        return nullptr;
+    }
+    else{
         return &iter->second.front();
     }
 }
 
-void SymbolTable::enterScope() { currLevel += 1; }
-void SymbolTable::leaveScope() { currLevel -= 1; }
-
-void SymbolTable::printTable() {
-    unordered_map<string, list<TableItem>>::iterator iter;
-    list<TableItem>::iterator                        iter2;
-
-    cout << "--------Proc--------" << endl;
-    for (iter = ProcDeclMap.begin(); iter != ProcDeclMap.end(); iter++) {
-        cout << iter->first;
-        for (iter2 = iter->second.begin(); iter2 != iter->second.end();
-             iter2++) {
-            cout << iter2->toString() << endl;
-        }
-    }
-    cout << "--------Func--------" << endl;
-    for (iter = FuncDeclMap.begin(); iter != FuncDeclMap.end(); iter++) {
-        cout << iter->first;
-        for (iter2 = iter->second.begin(); iter2 != iter->second.end();
-             iter2++) {
-            cout << iter2->toString() << endl;
-        }
-    }
-    cout << "--------Var--------" << endl;
-    for (iter = VarDeclMap.begin(); iter != VarDeclMap.end(); iter++) {
-        cout << iter->first;
-        for (iter2 = iter->second.begin(); iter2 != iter->second.end();
-             iter2++) {
-            cout << iter2->toString() << endl;
-        }
-    }
-    cout << "--------Type--------" << endl;
-    for (iter = TypeDeclMap.begin(); iter != TypeDeclMap.end(); iter++) {
-        cout << iter->first;
-        for (iter2 = iter->second.begin(); iter2 != iter->second.end();
-             iter2++) {
-            cout << iter2->toString() << endl;
-        }
-    }
+void SymbolTable::enterScope(){
+    printTable();
+    currLevel+=1;
 }
+void SymbolTable::leaveScope(){
+    printTable();
+    unordered_map<string, list<TableItem> >::iterator iter;
+    iter=ProcDeclMap.begin();
+    while(iter!=ProcDeclMap.end()){
+        if(iter->second.size()>0 && iter->second.front().level==currLevel){
+            iter->second.pop_front();
+        }
+        if(iter->second.size()==0){
+            iter=ProcDeclMap.erase(iter);
+        }
+        else{
+            iter++;
+        }
+    }
+    iter=FuncDeclMap.begin();
+    while(iter!=FuncDeclMap.end()){
+        if(iter->second.size()>0 && iter->second.front().level==currLevel){
+            iter->second.pop_front();
+        }
+        if(iter->second.size()==0){
+            iter=FuncDeclMap.erase(iter);
+        }
+        else{
+            iter++;
+        }
+    }
+    iter=VarDeclMap.begin();
+    while(iter!=VarDeclMap.end()){
+        if(iter->second.size()>0 && iter->second.front().level==currLevel){
+            iter->second.pop_front();
+        }
+        if(iter->second.size()==0){
+            iter=VarDeclMap.erase(iter);
+        }
+        else{
+            iter++;
+        }
+    }
+    iter=TypeDeclMap.begin();
+    while(iter!=TypeDeclMap.end()){
+        if(iter->second.size()>0 && iter->second.front().level==currLevel){
+            iter->second.pop_front();
+        }
+        if(iter->second.size()==0){
+            iter=TypeDeclMap.erase(iter);
+        }
+        else{
+            iter++;
+        }
+    }
+    currLevel-=1;
+}
+
+void SymbolTable::printTable(){
+    cout<<"current level: "<<to_string(currLevel)<<endl;
+
+    unordered_map<string, list<TableItem> >::iterator iter;
+    list<TableItem>::iterator iter2;
+
+    cout<<"--------Proc--------"<<endl;    
+    for(iter=ProcDeclMap.begin();iter!=ProcDeclMap.end();iter++){
+        cout<<iter->first;
+        for(iter2=iter->second.begin(); iter2!=iter->second.end(); iter2++){
+            cout<<iter2->toString()<<endl;
+        }
+    }
+    cout<<"--------Func--------"<<endl;    
+    for(iter=FuncDeclMap.begin();iter!=FuncDeclMap.end();iter++){
+        cout<<iter->first;
+        for(iter2=iter->second.begin(); iter2!=iter->second.end(); iter2++){
+            cout<<iter2->toString()<<endl;
+        }
+    }
+    cout<<"--------Var--------"<<endl;    
+    for(iter=VarDeclMap.begin();iter!=VarDeclMap.end();iter++){
+        cout<<iter->first;
+        for(iter2=iter->second.begin(); iter2!=iter->second.end(); iter2++){
+            cout<<iter2->toString()<<endl;
+        }
+    }
+    cout<<"--------Type--------"<<endl;    
+    for(iter=TypeDeclMap.begin();iter!=TypeDeclMap.end();iter++){
+        cout<<iter->first;
+        for(iter2=iter->second.begin(); iter2!=iter->second.end(); iter2++){
+            cout<<iter2->toString()<<endl;
+        }
+    }
+    cout<<endl;
+}
+
